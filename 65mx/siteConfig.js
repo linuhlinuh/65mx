cd1e51e1bcb2bfc03f797e5c7294bed197db768a
linuhlinuh <linuhlinuh@gmail.com>
2021-05-10 20:38:37 +0300

    init

---
+++ 65mx/siteConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: '65mx',
+  shortName: '65mx',
+  description: 'MX School Kharkov',
+  url: 'https://gatsby-london.netlify.com', // Domain of your site without prefix!
+  prefix: '/',
+  author: 'Imed Adel',
+  twitter: 'Imed_Adel'
+}
